91de8ab437083910f37b2a616a5c50c3defac4e9
home <home@lan.home>
2025-05-07 23:11:32 +1000

    take 2

--- parameters/vendor/wapacro/az-keyvault-php/src/Responses/Secret/SecretAttributeEntity.php
+++ parameters/vendor/wapacro/az-keyvault-php/src/Responses/Secret/SecretAttributeEntity.php
@@ -6,9 +6,26 @@ use AzKeyVault\Contracts\AttributeInterface;
 
 class SecretAttributeEntity implements AttributeInterface {
     public $enabled;
+    public $created;
+    public $updated;
+    public $recoveryLevel;
+    public $exp;
+    public $nbf;
 
-    public function __construct(array $data = []) {
-        $this->enabled = $data['enabled'] ?? null;
+    public function __construct(
+        $enabled = null,
+        $created = null,
+        $updated = null,
+        $recoveryLevel = null,
+        $exp = null,
+        $nbf = null
+    ) {
+        $this->enabled = $enabled;
+        $this->created = $created;
+        $this->updated = $updated;
+        $this->recoveryLevel = $recoveryLevel;
+        $this->exp = $exp;
+        $this->nbf = $nbf;
     }
 
     public function isEnabled() {
